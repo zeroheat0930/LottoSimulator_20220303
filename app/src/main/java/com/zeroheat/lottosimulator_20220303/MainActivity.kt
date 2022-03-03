5d1844ef5f183b431e8c8d36168173d161048d5c
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-03 10:48:50 +0900

    ArrayList로 당첨번호 목록 변수 추가, 실제 당첨번호 목록 저장 기능

--- app/src/main/java/com/zeroheat/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/com/zeroheat/lottosimulator_20220303/MainActivity.kt
@@ -6,6 +6,11 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    컴퓨터가 뽑은 당첨번호 6개를 저장할 ArrayList(배열)
+    val mWinNumberList = ArrayList<Int>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +31,13 @@ class MainActivity : AppCompatActivity() {
 
 //        6개의 당첨번호 생성
 //        코틀린의 for문은, for-each 문법으로 기반.
+
+//        ArrayList는 목록을 계속 누적 가능.
+//        당첨번호 뽑기 전에, 기존의 당첨번호는 전부 삭제하고 다시 뽑자.
+
+        mWinNumberList.clear()
+
+
         for ( i in 0 until 6 ){
 
 //            괜찮은 번호가 나올때 까지 무한 반복
@@ -37,7 +49,13 @@ class MainActivity : AppCompatActivity() {
 
 //                중복 검사 통과 시 while 깨자
 
-                if(true){
+
+
+                if( !mWinNumberList.contains(randomNum)){
+
+//                    당첨번호로, 뽑은 랜덤 숫자 등록
+                    mWinNumberList.add(randomNum)
+
                     break
                 }
 
@@ -46,6 +64,11 @@ class MainActivity : AppCompatActivity() {
         }
 
 //        만들어진 당첨번호 6개를 -> 텍스트뷰에 표현
+        Log.d("당첨번호목록", mWinNumberList.toString())
+
+            for(winNUm in mWinNumberList){
+
+            }
 
 //        보너스번호 생성
 
