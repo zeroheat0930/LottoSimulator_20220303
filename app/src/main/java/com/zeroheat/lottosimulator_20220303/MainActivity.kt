23d3d15ab5f945852df18d129baccd439e668984
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-03 14:24:21 +0900

    숫자 오름차순 정렬, 당첨번호 목록 > 텍스트뷰 목록에 반영, 보너스 번호 생성 로직

--- app/src/main/java/com/zeroheat/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/com/zeroheat/lottosimulator_20220303/MainActivity.kt
@@ -3,12 +3,17 @@ package com.zeroheat.lottosimulator_20220303
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-//    컴퓨터가 뽑은 당첨번호 6개를 저장할 ArrayList(배열)
+    //    컴퓨터가 뽑은 당첨번호 6개를 저장할 ArrayList
     val mWinNumberList = ArrayList<Int>()
+    var mBonusNum = 0 // 보너스번호는, 매 판마다 새로 뽑아야함. 변경소지 O, 화면이 어딘지는 줄 필요 X. 바로 대입 var
+
+    //    당첨번호를 보여줄 6개의 텍스트뷰를 담아둘 ArrayList
+    val mWinNumTextViewList = ArrayList<TextView>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun buyLotto(){
+    private fun buyLotto() {
 
 //        6개의 당첨번호 생성
 //        코틀린의 for문은, for-each 문법으로 기반.
@@ -37,25 +42,18 @@ class MainActivity : AppCompatActivity() {
 
         mWinNumberList.clear()
 
-
-        for ( i in 0 until 6 ){
+        for ( i  in 0 until 6  ) {
 
 //            괜찮은 번호가 나올때 까지 무한 반복
-            while (true){
+            while (true) {
 
-//                1~45의 랜덤 숫자
+//                1 ~ 45의 랜덤 숫자
 //                Math.random()은 0~1 => 1 ~ 45.xxx 로 가공 => Int로 캐스팅
-                val randomNum = (Math.random() * 45 + 1).toInt()
-
+                val randomNum =  (Math.random() * 45 + 1).toInt()
 //                중복 검사 통과 시 while 깨자
-
-
-
-                if( !mWinNumberList.contains(randomNum)){
-
+                if ( !mWinNumberList.contains(randomNum) ) {
 //                    당첨번호로, 뽑은 랜덤 숫자 등록
-                    mWinNumberList.add(randomNum)
-
+                    mWinNumberList.add( randomNum )
                     break
                 }
 
@@ -63,19 +61,50 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-//        만들어진 당첨번호 6개를 -> 텍스트뷰에 표현
+//        만들어진 당첨번호 6개를 -> 작은수 ~ 큰 수로 정리해서 -> 텍스트뷰에 표현
+
+        mWinNumberList.sort() // 자바로 직접 짜던 로직을 > 객체지향의 특성, 만들어져있는 기능 활용으로 대체.
+
         Log.d("당첨번호목록", mWinNumberList.toString())
 
-            for(winNUm in mWinNumberList){
+//        for > 돌면서, 당첨번호도 / 몇번째 바퀴인지도 필요 => 텍스트뷰를 찾아내야함.
 
+        mWinNumberList.forEachIndexed { index, winNum ->
+
+//            순서에 맞는 텍스트뷰 추출 => 문구로 당첨번호 설정
+
+            mWinNumTextViewList[index].text = winNum.toString()
+
+        }
+
+//        보너스번호 생성 -> 1~45 하나, 당첨번호와 겹치지 않게.
+
+        while (true) {
+            val randomNum = (Math.random() * 45 + 1).toInt()
+
+            if (!mWinNumberList.contains(randomNum)) {
+//                겹치지 않는 숫자 뽑음.
+
+                mBonusNum = randomNum
+                break
             }
 
-//        보너스번호 생성
+        }
 
 //        텍스트뷰에 배치
+
+        txtBonusNum.text = mBonusNum.toString()
+
     }
 
-    private fun setValues(){
+    private fun setValues() {
+
+        mWinNumTextViewList.add(txtWinNum01)
+        mWinNumTextViewList.add(txtWinNum02)
+        mWinNumTextViewList.add(txtWinNum03)
+        mWinNumTextViewList.add(txtWinNum04)
+        mWinNumTextViewList.add(txtWinNum05)
+        mWinNumTextViewList.add(txtWinNum06)
 
     }
 
