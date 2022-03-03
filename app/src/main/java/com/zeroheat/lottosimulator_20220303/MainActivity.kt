874a143526aa2e910d446b13c5fc7074fea36bc8
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-03 10:39:18 +0900

    6개의 랜덤 숫자 뽑기

--- app/src/main/java/com/zeroheat/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/com/zeroheat/lottosimulator_20220303/MainActivity.kt
@@ -26,8 +26,23 @@ class MainActivity : AppCompatActivity() {
 
 //        6개의 당첨번호 생성
 //        코틀린의 for문은, for-each 문법으로 기반.
-        for ( i in 0..5 ){
-            Log.d("반복문확인", i.toString())
+        for ( i in 0 until 6 ){
+
+//            괜찮은 번호가 나올때 까지 무한 반복
+            while (true){
+
+//                1~45의 랜덤 숫자
+//                Math.random()은 0~1 => 1 ~ 45.xxx 로 가공 => Int로 캐스팅
+                val randomNum = (Math.random() * 45 + 1).toInt()
+
+//                중복 검사 통과 시 while 깨자
+
+                if(true){
+                    break
+                }
+
+            }
+
         }
 
 //        만들어진 당첨번호 6개를 -> 텍스트뷰에 표현
